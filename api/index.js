ca7d62c2820031e1fa3be27eb505733e0051a518
provisioncons <52004741+provisioncons@users.noreply.github.com>
2025-06-04 15:46:28 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,5 +4,6 @@ export default function handler(req, res) {
   const mailto = `mailto:provision@provision.com.br?subject=${encodeURIComponent(assunto)}&body=${encodeURIComponent(descricao)}`;
 
   res.writeHead(302, { Location: mailto });
-  res.end();
+  res.end(`Redirecionando para: ${mailto}`);
 }
+
